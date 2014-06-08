37fda1a3fe860e8e1355a516629088180c153b2a
Ben Foxall <benfoxall@gmail.com>
2014-06-08 22:04:49 +0200

    manually satisfy transform test

--- src/jumbotron.js
+++ src/jumbotron.js
@@ -6,3 +6,18 @@ jumbotron.observe = function(observation){
 };
 
 jumbotron.construct = function(){}
+
+
+jumbotron.util = {};
+
+jumbotron.util.mapToUnit = function(corners) {
+	// find a transform that will convert corners to
+	// 0,0 - 1,0 - 1,1 - 0,1
+
+	// manual to match test value
+	return $M([
+		[1/100, 0,     0],
+		[0,     1/100, -1.5],
+		[0,     0,     1]])
+
+}
