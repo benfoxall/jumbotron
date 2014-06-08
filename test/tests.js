0ff0213cbcbf0efaf71d9740e04849c70f171eb3
Ben Foxall <benfoxall@gmail.com>
2014-06-08 22:04:15 +0200

    added utility test

--- test/tests.js
+++ test/tests.js
@@ -36,3 +36,43 @@ describe('recognition', function(){
 	it('positioned markers')
 
 })
+
+describe('utils', function(){
+
+	describe('mapToUnit', function(){
+		var transform,
+			corners = detections.a[0].corners,
+			cornerVs = corners.map(function(c){return $V([c.x,c.y,1])});
+
+		before(function(){
+			transform = jumbotron.util.mapToUnit(corners)
+		});
+
+		it('returned a matrix', function(){
+			transform.should.be.an.instanceOf(Matrix);
+		})
+		it('maps corner 0 to 0,0', function(){
+			match_point(transform.x(cornerVs[0]), [0,0], .5)
+		})
+
+		it('maps corner 1 to 1,0', function(){
+			match_point(transform.x(cornerVs[1]), [1,0], .5)
+		})
+
+		it('maps corner 2 to 1,1', function(){
+			match_point(transform.x(cornerVs[2]), [1,1], .5)
+		})
+
+		it('maps corner 3 to 0,1', function(){
+			match_point(transform.x(cornerVs[3]), [0,1], .5)
+		})
+
+	})
+
+})
+
+
+function match_point(vector, array, accuracy){
+	vector.elements[0].should.be.approximately(array[0],accuracy)
+	vector.elements[1].should.be.approximately(array[1],accuracy)
+}
